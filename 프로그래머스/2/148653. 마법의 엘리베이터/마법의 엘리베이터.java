3eeb4b0d8af8356879e45b10c58c91ae24ad8521
Junyoung <83858869+junyoung-choe@users.noreply.github.com>
2024-05-11 12:11:08 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        
+        while(storey != 0) {
+            int now = storey % 10;
+            storey /= 10;
+            
+            if (now > 5) {
+                // 이 부분이 틀린 요인 1
+                // answer += (10-now) + 1;
+                answer += (10-now);
+                storey++;
+            } else if (now < 5){
+                answer += now;
+            } else if (storey % 10 >= 5) {
+                // 십의 자리 숫자의 연산을 줄여준다 !  이건 십의자릴 숫자 ++해준는게 이득
+                answer += 5;
+                storey++;
+            } else {
+                // 즉 이건 십의 자리 숫자를 그대로 두는 것이 이득
+                answer += 5;
+            }
+        }
+        
+        return answer;
+    }
+}
+
+// 그리디 문제인건 감이왔지만
+// 각 자리만 계산하면 된다고 생각했다 ! 
+// 1의 자리 숫자가 5인 경우에는 두가지를 더 고려해야 한다
+// 15 와 55인 경우를 생각해보자 
